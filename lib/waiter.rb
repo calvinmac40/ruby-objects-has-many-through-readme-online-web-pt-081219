0bc8ce054002e7ae9341c5257d034df7c41a7e63
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-05 12:58:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,9 @@
 class Waiter
-
+  attr_accessor :years_of_experience
+  attr_accessor :name 
+  
+  def initialize(name,years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+  end
 end
