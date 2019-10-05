605da61a715b3394592ef6425d1941a987ef2dee
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-05 12:59:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,16 @@
 class Waiter
   attr_accessor :years_of_experience
-  attr_accessor :name 
+  attr_accessor :name
+  
+  @@all = []
   
   def initialize(name,years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @@all << self
+  end
+  
+  def self.all
+    
   end
 end
