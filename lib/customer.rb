9b96872041d7e2c7049a197ddbb6fea4086038d9
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-05 12:54:21 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  attr_accessor :age
+  attr_accessor :name
+  
+  @@all = []
+  @@counter = 0
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @years_of_experience = []
+    @@all << self
+  end
+  
   
 end
