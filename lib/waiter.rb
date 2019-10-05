bb21387102ef8f242f66ebe7e7f6ca9db8f2c649
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-05 14:10:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,7 +16,13 @@ class Waiter
     end
   end
   
-  def 
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a,meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    
+    best_tipped_meal.customer
+  end
   
   def self.all
     @@all
